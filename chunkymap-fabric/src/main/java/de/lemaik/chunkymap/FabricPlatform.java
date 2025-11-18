94b392d9ba58d80c363d8a841a0ac343341f427d
RedPolygon <dantevg006@gmail.com>
2025-11-18 21:48:54 +0100

    Update for 1.21.10

--- chunkymap-fabric/src/main/java/de/lemaik/chunkymap/FabricPlatform.java
+++ chunkymap-fabric/src/main/java/de/lemaik/chunkymap/FabricPlatform.java
@@ -17,6 +17,6 @@ public class FabricPlatform extends Platform {
 	
 	@Override
 	public String getMinecraftVersion() {
-		return MinecraftVersion.CURRENT.name();
+		return MinecraftVersion.create().name();
 	}
 }
